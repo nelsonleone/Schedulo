3a1870ba0d7741a7b5352de754e84c42de937974
Onuegbu Nelson <95982650+nelsonleone@users.noreply.github.com>
2024-11-10 15:12:17 +0100

    update to font authpanel

--- src/lib/components/layout-components/AuthPanel.svelte
+++ src/lib/components/layout-components/AuthPanel.svelte
@@ -6,16 +6,16 @@
 </script>
 
 <form method="post" aria-labelledby="signin-form" class="flex flex-col gap-4 items-center justify-center mt-16">
-    <label id="signin-form" for="" class="text-sm md:text-base">Continue With</label>
+    <label id="signin-form" for="" class="text-base">Continue With</label>
 
     <Button type="submit" formaction={`${formAction}&provider=google`} 
-            class="font-semibold flex gap-2 font-quicksand w-40 md:w-48 md:text-lg md:py-3 md:h-auto" aria-label="Continue with Google">
+            class="font-semibold flex gap-2 font-quicksand w-48 md:text-lg md:py-3 md:h-auto" aria-label="Continue with Google">
         <Icon class="text-2xl" icon="flat-color-icons:google" />
         <span>Google</span>
     </Button>
 
     <Button type="submit" formaction={`${formAction}&provider=facebook`} 
-            class="font-semibold flex gap-2 font-quicksand w-40 md:w-48 md:text-lg md:py-3 md:h-auto" aria-label="Continue with Facebook">
+            class="font-semibold flex gap-2 font-quicksand w-48 md:text-lg md:py-3 md:h-auto" aria-label="Continue with Facebook">
         <Icon class="text-2xl" icon="logos:facebook" />
         <span>Facebook</span>
     </Button>
